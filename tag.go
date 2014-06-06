3dd212453653bd2f382c0d73e13f52615da187ed
Dave Cheney <dave@cheney.net>
2014-06-06 17:26:47 +1000

    unexport TagKind

--- tag.go
+++ tag.go
@@ -15,7 +15,7 @@ type Tag interface {
 
 // TagKind returns one of the *TagKind constants for the given tag, or
 // an error if none matches.
-func TagKind(tag string) (string, error) {
+func tagKind(tag string) (string, error) {
 	i := strings.Index(tag, "-")
 	if i <= 0 || !validKinds(tag[:i]) {
 		return "", fmt.Errorf("%q is not a valid tag", tag)
@@ -32,7 +32,7 @@ func validKinds(kind string) bool {
 }
 
 func splitTag(tag string) (kind, rest string, err error) {
-	kind, err = TagKind(tag)
+	kind, err = tagKind(tag)
 	if err != nil {
 		return "", "", err
 	}
