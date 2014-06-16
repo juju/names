88e2a0ddffb2d3125dafb8afc8149951fa53a1f0
John Weldon <johnweldon4@gmail.com>
2014-06-16 14:49:07 -0700

    Add Tag type for Action

---
+++ action.go
@@ -0,0 +1,53 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package names
+
+import (
+	"regexp"
+)
+
+const (
+	ActionTagKind = "action"
+	ActionMarker  = "_a_"
+)
+
+const (
+	validActionNameRegex = ServiceSnippet + "([-/]" + NumberSnippet + ")?" + ActionMarker + NumberSnippet
+)
+
+var validAction = regexp.MustCompile("^" + validActionNameRegex + "$")
+
+// IsAction returns whether name is a valid action name.
+// Valid action names include the names.ActionMarker token that delimits
+// a prefix that can be used for filtering, and a suffix that should be
+// unique.  The prefix should match the name rules for units and services
+func IsAction(name string) bool {
+	return validAction.MatchString(name)
+}
+
+type ActionTag struct {
+	name string
+}
+
+func (t ActionTag) String() string { return t.Kind() + "-" + t.Id() }
+func (t ActionTag) Kind() string   { return ActionTagKind }
+func (t ActionTag) Id() string     { return t.name }
+
+// NewActionTag returns the tag for the action with the given name.
+func NewActionTag(actionName string) Tag {
+	return ActionTag{name: actionName}
+}
+
+// ParseActionTag parses a action tag string.
+func ParseActionTag(actionTag string) (ActionTag, error) {
+	tag, err := ParseTag(actionTag)
+	if err != nil {
+		return ActionTag{}, err
+	}
+	st, ok := tag.(ActionTag)
+	if !ok {
+		return ActionTag{}, invalidTagError(actionTag, ActionTagKind)
+	}
+	return st, nil
+}
