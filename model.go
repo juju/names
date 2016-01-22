3e917b37166efaa1570066b7b05a43bc332bdd1a
Ian Booth <ian.booth@canonical.com>
2016-01-22 14:38:15 +1000

    Fix stupid name

--- model.go
+++ model.go
@@ -9,34 +9,34 @@ import (
 
 const ModelTagKind = "model"
 
-// EnvironModelTag represents a tag used to describe a model.
-type EnvironModelTag struct {
+// EnvironModel represents a tag used to describe a model.
+type EnvironModel struct {
 	uuid string
 }
 
 var validUUID = regexp.MustCompile(`[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}`)
 
 // NewModelTag returns the tag of an model with the given model UUID.
-func NewModelTag(uuid string) EnvironModelTag {
-	return EnvironModelTag{uuid: uuid}
+func NewModelTag(uuid string) EnvironModel {
+	return EnvironModel{uuid: uuid}
 }
 
 // ParseModelTag parses an environ tag string.
-func ParseModelTag(ModelTag string) (EnvironModelTag, error) {
-	tag, err := ParseTag(ModelTag)
+func ParseModelTag(modelTag string) (EnvironModel, error) {
+	tag, err := ParseTag(modelTag)
 	if err != nil {
-		return EnvironModelTag{}, err
+		return EnvironModel{}, err
 	}
-	et, ok := tag.(EnvironModelTag)
+	et, ok := tag.(EnvironModel)
 	if !ok {
-		return EnvironModelTag{}, invalidTagError(ModelTag, ModelTagKind)
+		return EnvironModel{}, invalidTagError(modelTag, ModelTagKind)
 	}
 	return et, nil
 }
 
-func (t EnvironModelTag) String() string { return t.Kind() + "-" + t.Id() }
-func (t EnvironModelTag) Kind() string   { return ModelTagKind }
-func (t EnvironModelTag) Id() string     { return t.uuid }
+func (t EnvironModel) String() string { return t.Kind() + "-" + t.Id() }
+func (t EnvironModel) Kind() string   { return ModelTagKind }
+func (t EnvironModel) Id() string     { return t.uuid }
 
 // IsValidModel returns whether id is a valid model UUID.
 func IsValidModel(id string) bool {
