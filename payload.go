199aba21c418e870411b2591729a73789a40eb1c
Eric Snow <ericsnowcurrently@gmail.com>
2016-05-20 14:05:46 -0600

    Use the payload class name as the ID.

--- payload.go
+++ payload.go
@@ -4,6 +4,8 @@
 package names
 
 import (
+	"regexp"
+
 	"github.com/juju/utils"
 )
 
@@ -11,12 +13,17 @@ const (
 	// PayloadTagKind is used as the prefix for the string
 	// representation of payload tags.
 	PayloadTagKind = "payload"
+
+	payloadClass = "([a-zA-Z][a-zA-Z0-9]*)"
 )
 
+var validPayload = regexp.MustCompile("^" + payloadClass + "$")
+
 // IsValidPayload returns whether id is a valid Juju ID for
-// a charm payload. The ID must be a valid UUID.
+// a charm payload. The ID must be a valid "identifier". For
+// compatibility with Juju 1.25, UUIDs are also supported.
 func IsValidPayload(id string) bool {
-	return utils.IsValidUUIDString(id)
+	return validPayload.MatchString(id) || utils.IsValidUUIDString(id)
 }
 
 // PayloadTag represents a charm payload.
