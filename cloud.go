40b40d381af374a96d8c5440a70a4b11ac2ee8f9
Tim McNamara <tim.mcnamara@canonical.com>
2019-03-15 16:55:12 +1300

    Give clouds minimum and maximum lengths
    This commit restricts the range of valid cloud names to [1, 4096]

--- cloud.go
+++ cloud.go
@@ -11,7 +11,7 @@ import (
 const CloudTagKind = "cloud"
 
 var (
-	cloudSnippet = "[a-zA-Z0-9][a-zA-Z0-9._-]*"
+	cloudSnippet = "[a-zA-Z0-9][a-zA-Z0-9._-]{1,4096}"
 	validCloud   = regexp.MustCompile("^" + cloudSnippet + "$")
 )
 
